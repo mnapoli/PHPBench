2921a983dc221d3585929cf127e56b5b67a34432
Matthieu Napoli <matthieu@mnapoli.fr>
2011-04-10 13:56:23 +0200

    Closes #3. Closes #4.

--- library/PHPBench/Runner.php
+++ library/PHPBench/Runner.php
@@ -5,6 +5,8 @@
 
 namespace PHPBench;
 
+require_once 'BenchCase/DryRun.php';
+
 /**
  * Bench runner
  */
@@ -20,23 +22,64 @@ class Runner
      */
     public function run(BenchCase $benchCase)
     {
+        // Calibration
+        $baseTime = $this->calibrate($benchCase->getIterationNumber());
+        // Run the bench
         $results = $benchCase->run();
         // Render the results
-        $this->displayResults($results);
+        $this->displayResults($results, $baseTime);
+    }
+
+    /**
+     * Calibrate the bench
+     *
+     * Run an empty test
+     *
+     * @param int $iterationNumber Number of iteration
+     *
+     * @return float Execution time of an empty bench
+     */
+    public function calibrate($iterationNumber)
+    {
+        $benchCase = new BenchCase\DryRunBench();
+        $benchCase->setIterationNumber($iterationNumber);
+        $results = $benchCase->run();
+        return array_pop($results);
     }
 
     /**
      * Display the results
      *
-     * @param array $results Results
+     * @param array $results  Results
+     * @param float $baseTime Base time of an empty bench
      *
      * @return void
      */
-    protected function displayResults($results)
+    protected function displayResults($results, $baseTime)
     {
+        // Substract the base time
         foreach ($results as $benchStep => $time) {
+            $results[$benchStep] -= $baseTime;
+            if ($results[$benchStep] < 0) {
+                $results[$benchStep] = 0.;
+            }
+        }
+        // Find the lowest time
+        $lowest = null;
+        foreach ($results as $benchStep => $time) {
+            if (($lowest === null) || ($lowest > $time)) {
+                $lowest = $time;
+            }
+        }
+        // Display
+        foreach ($results as $benchStep => $time) {
+            if ($lowest > 0) {
+                $percentage = number_format($time / $lowest * 100, 0);
+            } else {
+                $percentage = 100;
+            }
             $time = number_format($time, 3);
-            echo $benchStep.' : '.$time.' ms'.PHP_EOL;
+            echo $benchStep.' : '.$time.' ms - '.$percentage.' %'.PHP_EOL;
         }
     }
 
